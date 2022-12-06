340ae151ab288c2124718f266980664578be31d9
Borbdein <bordbein@tutanota.com>
2022-12-06 23:16:06 +0100

    completed day 3 part 1

---
+++ task_3.ps1
@@ -0,0 +1,31 @@
+﻿$folderRoot = Split-Path $MyInvocation.MyCommand.Source
+$taskInput = Get-Content -Path (Join-Path $folderRoot "inputs\input_3.txt")
+
+$chars = @()
+$prio = 0
+
+$chars += ([char]'a'..[char]'z')
+$chars += ([char]'A'..[char]'Z')
+
+foreach($line in ($taskInput -split "`r`n")) {
+    $lineMid = $line.Length / 2
+    $lineRest = $line.Length - $lineMid
+    $firstComp = $line.Substring(0, $lineMid)
+    $secComp = $line.Substring(($lineMid), $lineRest)
+
+    $matchingLetters = @()
+
+    foreach($letter in $firstComp.ToCharArray()) {
+        if($secComp -cmatch $letter) {
+            $matchingLetters += $letter
+        }
+    }
+
+    $theMatchingLetters = $matchingLetters | select -Unique
+
+    foreach($matLetter in $theMatchingLetters) {
+        $prio += ($chars.IndexOf([int]([byte][char]$matLetter)) + 1)
+    }
+}
+
+Write-Host ('Task 3, part 1: {0}' -f $prio)
