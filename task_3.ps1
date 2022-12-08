0c8d36057bdbe235b757cb6d195b36a2b718776b
Borbdein <bordbein@tutanota.com>
2022-12-08 21:11:22 +0100

    finished task 3 part 2

--- task_3.ps1
+++ task_3.ps1
@@ -3,14 +3,18 @@ $taskInput = Get-Content -Path (Join-Path $folderRoot "inputs\input_3.txt")
 
 $chars = @()
 $prio = 0
+$prioPartTwo = 0
 
 $chars += ([char]'a'..[char]'z')
 $chars += ([char]'A'..[char]'Z')
 
-foreach($line in ($taskInput -split "`r`n")) {
+$theLines = ($taskInput -split "`r`n")
+
+foreach($line in $theLines) {
     $lineMid = $line.Length / 2
+    $lineRest = $line.Length - $lineMid
     $firstComp = $line.Substring(0, $lineMid)
-    $secComp = $line.Substring($lineMid, $lineMid)
+    $secComp = $line.Substring(($lineMid), $lineRest)
 
     $matchingLetters = @()
 
@@ -31,6 +35,24 @@ Write-Host ('Task 3, part 1: {0}' -f $prio)
 
 
 
-foreach($line in ($taskInput -split "`r`n")) {
+
+for($i=0;$i -lt $theLines.Length;$i = ($i + 3)) {
     
+    $checkLine = $theLines[$i]
+
+    $matchingLetters = @()
+
+    foreach($letter in $checkLine.ToCharArray()) {
+        if(($theLines[$i+1] -cmatch $letter) -and ($theLines[$i+2] -cmatch $letter) ) {
+            $matchingLetters += $letter
+        }
+    }
+
+    $theMatchingLetters = $matchingLetters | select -Unique
+
+    foreach($matLetter in $theMatchingLetters) {
+        $prioPartTwo += ($chars.IndexOf([int]([byte][char]$matLetter)) + 1)
+    }
 }
+
+Write-Host ('Task 3, part 2: {0}' -f $prioPartTwo)
