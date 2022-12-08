4f805892ffcba96fa233383d083551f731890f1e
Borbdein <bordbein@tutanota.com>
2022-12-08 22:09:56 +0100

    finished task 4 part 2

--- task_4.ps1
+++ task_4.ps1
@@ -3,6 +3,7 @@ $taskInput = Get-Content -Path (Join-Path $folderRoot "inputs\input_4.txt")
 
 $theLines = ($taskInput -split "`r`n")
 $numberOfSectionsInSections = 0
+$numberofOverlapping = 0
 
 foreach($line in $theLines) {
     $sections = @()
@@ -26,3 +27,23 @@ foreach($line in $theLines) {
 }
 
 Write-Host ('Task 4, part 1: {0}' -f $numberOfSectionsInSections)
+
+
+
+:lineLoop foreach($line in $theLines) {
+    $sections = @()
+
+    $splitLine = $line.Split(',')
+
+    $sections += ,(($splitLine[0].Split('-')[0])..($splitLine[0].Split('-')[1]))
+    $sections += ,(($splitLine[1].Split('-')[0])..($splitLine[1].Split('-')[1]))
+
+    foreach($number in $sections[0]) {
+        if($number -in $sections[1]){
+            $numberofOverlapping++
+            continue lineLoop
+        }
+    }
+}
+
+Write-Host ('Task 4, part 2: {0}' -f $numberofOverlapping)
