871a3849d6777d1d9586a457a47b1dc4d26fec58
Kjetil Houlder <kjetil.houlder@ikomm.no>
2022-12-10 09:16:19 +0100

    started task 5

---
+++ task_5.ps1
@@ -0,0 +1,19 @@
+﻿$folderRoot = Split-Path $MyInvocation.MyCommand.Source
+$taskInput = Get-Content -Path (Join-Path $folderRoot "inputs\input_5.txt")
+
+$theLines = ($taskInput -split "`r`n")
+
+$caseArray = @()
+
+foreach($line in ($theLines | select -First 1)) {
+    $lineChars = $line.ToCharArray()
+
+    Write-Host ('Line: {0}' -f $line)
+
+    $count = 1
+    foreach($char in $lineChars) {
+        Write-Host ('Char {0}: {1}' -f $count, $char)
+
+        $count++
+    }
+}
