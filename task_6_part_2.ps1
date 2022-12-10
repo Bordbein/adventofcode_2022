4bf1912806296a186fc370476b7bf9ec0aa241fb
Borbdein <bordbein@tutanota.com>
2022-12-10 12:50:52 +0100

    added solution for day 6 part 2

---
+++ task_6_part_2.ps1
@@ -0,0 +1,23 @@
+﻿#------------------------------Notes------------------------------------#
+# Task 6, Part 2
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+$folderRoot = Split-Path $MyInvocation.MyCommand.Source
+$taskInput = Get-Content -Path (Join-Path $folderRoot "inputs\input_6.txt")
+
+$indicator = 0
+
+for($i=0;$i -lt ($taskInput.Length - 3);$i++) {
+    
+    $checkString = $taskInput.Substring($i, 14)
+    $checkChars = $checkString.ToCharArray()
+
+    $nrOfUnique = ($checkChars | Select-Object -Unique).Count
+    
+    if($nrOfUnique -eq 14) {
+        $indicator = $i + 14
+        break
+    }
+}
+
+Write-Host ('Task 6, Part 2: {0}' -f $indicator)
