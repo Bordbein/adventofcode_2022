2ef9880b262eee1f33750389d376cc554d5cc3f8
Borbdein <bordbein@tutanota.com>
2022-12-10 10:43:54 +0100

    finished day 5 part 1

--- task_5.ps1
+++ task_5.ps1
@@ -3,17 +3,56 @@ $taskInput = Get-Content -Path (Join-Path $folderRoot "inputs\input_5.txt")
 
 $theLines = ($taskInput -split "`r`n")
 
-$caseArray = @()
+$arrayWidth = ($theLines[0].Length + 1) / 4
 
-foreach($line in ($theLines | select -First 1)) {
-    $lineChars = $line.ToCharArray()
+$caseArray = New-Object string[] $arrayWidth
 
-    Write-Host ('Line: {0}' -f $line)
 
-    $count = 1
-    foreach($char in $lineChars) {
-        Write-Host ('Char {0}: {1}' -f $count, $char)
+foreach($line in $theLines) {
 
-        $count++
+    if($line -match '\[') {
+        $lineChars = $line.ToCharArray()
+
+        $charLoc = 0
+
+        for($i=1;$i -lt $lineChars.Length; $i = $i + 4) {
+            if($lineChars[$i] -match '[A-Z]') {
+                $caseArray[$charLoc] += $lineChars[$i]
+            }
+            
+            $charLoc++
+        }
+    }
+
+    if($line -match 'move') { 
+        $rules = $line.Split(' ')
+
+        $nrOfCases = ([int]([string]$rules[1]))
+        $fromStack = ([int]([string]$rules[3]) - 1)
+        $toStack = ([int]([string]$rules[5]) - 1)
+
+        $fromString = $caseArray[$fromStack]
+        $toString = $caseArray[$toStack]
+
+        $moveString = $fromString.Substring(0,$nrOfCases)
+
+        $newFromString = $fromString.Substring($nrOfCases,($fromString.Length - $nrOfCases))
+
+        $toStringChar = $toString.ToCharArray()
+        [array]::Reverse($toStringChar)
+
+        $newToStringChar = $toStringChar + $moveString.ToCharArray()
+        [array]::Reverse($newToStringChar)
+
+        $caseArray[$fromStack] = $newFromString
+        $caseArray[$toStack] = [string](-join $newToStringChar)
     }
 }
+
+$topCases = [string]::Empty
+
+foreach($stack in $caseArray) {
+    $topCases += $stack.Substring(0,1)
+}
+
+Write-Host ('Task 4, part 1: {0}' -f $topCases)
